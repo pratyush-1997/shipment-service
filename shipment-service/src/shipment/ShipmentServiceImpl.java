e27f5f451fa8e688ea3da72aed3fcfface0e349c
pratyush-1997 <82928205+pratyush-1997@users.noreply.github.com>
2021-07-11 21:49:29 +0530

    3rd commit

---
+++ shipment-service/src/shipment/ShipmentServiceImpl.java
@@ -0,0 +1,22 @@
+package shipment;
+public class ShipmentServiceImpl implements IShipment {
+
+	@Override
+	public boolean addShipment() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteShipment() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean getShipment() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
